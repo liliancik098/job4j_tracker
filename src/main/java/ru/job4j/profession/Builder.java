ad9421307fff20ade44e469d1c0c2bf1b8cf7214
pletosu.lilian <lilianzau@mail.ru>
2020-12-13 11:23:09 +0200

    3. Реализация профессий в коде [#400645]

---
+++ src/main/java/ru/job4j/profession/Builder.java
@@ -0,0 +1,7 @@
+package ru.job4j.profession;
+
+public class Builder extends Engineer {
+    public Builder(int id, String company, String name, String surname, String education, String birthday) {
+        super(id, company, name, surname, education, birthday);
+    }
+}
--- src/main/java/ru/job4j/profession/Teacher.java
+++ src/main/java/ru/job4j/profession/Teacher.java
@@ -1,4 +1,4 @@
-package ru.job4j.Profession;
+package ru.job4j.profession;
 
 public class Teacher extends Profession {
     private String subject;
